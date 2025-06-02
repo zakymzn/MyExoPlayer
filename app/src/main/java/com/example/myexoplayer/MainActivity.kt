4a219165f8644bbffeec7a7f2ece5a4e9779ca92
zakymzn <mamurzakynurrokhman@gmail.com>
2025-06-02 14:41:28 +0700

    menambahkan audio ke dalam exoplayer

--- app/src/main/java/com/example/myexoplayer/MainActivity.kt
+++ app/src/main/java/com/example/myexoplayer/MainActivity.kt
@@ -19,9 +19,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
 
         val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
             exoPlayer.setMediaItem(videoItem)
+            exoPlayer.addMediaItem(audioItem)
             exoPlayer.prepare()
         }
         binding.playerView.player = player
