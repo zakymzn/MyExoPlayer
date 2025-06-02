ad0b8112c346d255d380d23ab1d9bcc591d45448
zakymzn <mamurzakynurrokhman@gmail.com>
2025-06-02 16:08:21 +0700

    latihan exoplayer dengan notification

--- app/src/main/java/com/example/myexoplayer/MainActivity.kt
+++ app/src/main/java/com/example/myexoplayer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myexoplayer
 
+import android.content.ComponentName
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,7 +10,10 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
 import com.example.myexoplayer.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.MoreExecutors
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,17 +24,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
-        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
-
-        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
-            exoPlayer.setMediaItem(videoItem)
-            exoPlayer.addMediaItem(audioItem)
-            exoPlayer.prepare()
-        }
-        binding.playerView.player = player
+//        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
+//        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
+//
+//        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+//            exoPlayer.setMediaItem(videoItem)
+//            exoPlayer.addMediaItem(audioItem)
+//            exoPlayer.prepare()
+//        }
+//        binding.playerView.player = player
 
         hideSystemUI()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
+        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+        controllerFuture.addListener(
+            { binding.playerView.player = controllerFuture.get() },
+            MoreExecutors.directExecutor()
+        )
     }
 
     private fun hideSystemUI() {
